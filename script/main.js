a6e47fd4bbe2ea6fb5856940eba1cbe87768e001
ElhamFadel <elham2000Fadel@gmail.com>
2021-07-14 16:22:27 +0300

    update product #10

--- script/main.js
+++ script/main.js
@@ -1,4 +1,14 @@
+const products = document.querySelector('#products');
+const searchButton = document.getElementById('search-button');
+const products = localStorage.getItem('products');
+searchButton.addEventListener('click', () =>{
+ const nameProduct =  searchButton.value;
+ createProductComponent(getProduct(nameProduct,products));
+ return false;
+})
+    
 const getProduct = (nameProduct,arrayOfData) => {
+    const searchIput = document.querySelector('#searchInput');
     let product = arrayOfData.filter(x => x.name.includes(nameProduct));
     return product;
 }
