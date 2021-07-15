5c8eaaed840e132f05d9361a4ad99372bd61557e
ElhamFadel <elham2000Fadel@gmail.com>
2021-07-16 00:51:10 +0300

    add product test

--- script/utils.js
+++ script/utils.js
@@ -1,14 +1,15 @@
 // this folder for all javascript function that will reusable
-// export const deletingProduct = (id, array) => array.filter((ele) =>  ele.id !== id);
-
-// export const addProduct = (products, product) => [...products, product];
- const getProduct = (nameProduct,arrayOfData) => {
+const deletingProduct = (id, array) => array.filter((ele) => ele.id !== id);
+const addProduct = (products, product) => [...products, product];
+const getProduct = (nameProduct, arrayOfData) => {
     const searchIput = document.querySelector('#searchInput');
     let product = arrayOfData.filter(x => x.name.includes(nameProduct));
     return product;
 }
 if (typeof module !== "undefined") {
-  module.exports = {
-    getProduct
-  };
+    module.exports = {
+        getProduct,
+        addProduct,
+        deletingProduct
+    };
 }
